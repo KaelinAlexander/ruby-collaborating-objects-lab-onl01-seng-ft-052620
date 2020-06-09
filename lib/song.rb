67294dc31453da3ab965479d92809e166507718b
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-09 20:54:26 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+require "pry"
+
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.new_by_filename(file)
+    name = file.split(" - ")[1]
+    song = self.new(name)
+    song.artist = file.split(" - ")[0]
+  end
+  
+  def artist_name=(name)
+    if Artist.find_or_create_by_name(name)
+      Artist.add_song(self.name)
+    else
+    self.artist = Artist.new(name)
+    end
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
